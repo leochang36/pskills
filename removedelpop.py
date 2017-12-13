950134660ba675505a88652619789cd8ffee31a3
leochang <31495587+leochang36@users.noreply.github.com>
2017-12-13 14:45:29 +0800

    Add files via upload

---
+++ removedelpop.py
@@ -0,0 +1,20 @@
+list=[1,2,3,4,5,6,3]
+print(list)
+list.remove(3)
+print(list)
+
+
+list_del=[1,2,3,4,5,6,3]
+print(list_del)
+del list_del[3]
+print(list_del)
+
+
+list_pop=[1,2,3,4,5,6,3]
+print(list_pop)
+list_pop.pop()
+print(list_pop)
+list_pop.pop(2)
+print(list_pop)
+
+
